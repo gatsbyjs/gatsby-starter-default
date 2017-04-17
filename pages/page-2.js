5dbafe7bb8131804e9a6b94848029e8a2b83ee20
Kyle Mathews <mathews.kyle@gmail.com>
2017-04-17 09:57:11 -0700

    Prefix links

--- pages/page-2.js
+++ pages/page-2.js
@@ -12,7 +12,7 @@ export default class Index extends React.Component {
           Hi people
         </h1>
         <p>Welcome to page 2</p>
-        <Link to="/">Go back to the homepage</Link>
+        <Link to={prefixLink('/')}>Go back to the homepage</Link>
       </div>
     )
   }
