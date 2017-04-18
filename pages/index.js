d59f7e13c82d3975ea3ee6d203e43cfb15d59bae
Kyle Mathews <mathews.kyle@gmail.com>
2017-04-18 09:59:14 -0700

    Add missing prefixLink

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default class Index extends React.Component {
         </h1>
         <p>Welcome to your new Gatsby site.</p>
         <p>Now go build something great.</p>
-        <Link to="/page-2/">Go to page 2</Link>
+        <Link to={prefixLink('/page-2/')}>Go to page 2</Link>
       </div>
     )
   }
