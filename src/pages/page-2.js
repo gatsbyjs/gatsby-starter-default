9d1ce946bb52111a738fac77d1512f0c4127f4c3
Kyle Mathews <mathews.kyle@gmail.com>
2017-07-08 09:40:48 -0700

    Use more sensible class name

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -2,7 +2,7 @@ import React from "react"
 import Link from "gatsby-link"
 import Helmet from "react-helmet"
 
-export default class Index extends React.Component {
+export default class Page2 extends React.Component {
   render() {
     return (
       <div>
