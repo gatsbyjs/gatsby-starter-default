5cea832b07f196a280c46f04a3aa5730d0fc09de
Kyle Mathews <mathews.kyle@gmail.com>
2017-07-11 21:38:44 -0700

    Actually import typography.css

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,6 +3,8 @@ import PropTypes from "prop-types"
 import Link from "gatsby-link"
 import Helmet from "react-helmet"
 
+import "../css/typography.css"
+
 export default class Template extends React.Component {
   static propTypes = {
     children: PropTypes.func,
