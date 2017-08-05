951a72dedc69d7ecd2b590a99685e97be1e11d3d
Kyle Mathews <mathews.kyle@gmail.com>
2017-08-04 17:32:55 -0700

    Up dependencies plus tweak CSS"

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -5,7 +5,7 @@ import Helmet from 'react-helmet'
 
 import './index.css'
 
-const Header = () => (
+const Header = () =>
   <div
     style={{
       background: 'rebeccapurple',
@@ -28,36 +28,32 @@ const Header = () => (
           }}
         >
           Gatsby
-            </Link>
+        </Link>
       </h1>
     </div>
   </div>
-)
 
-const TemplateWrapper = ({
-  children
-}) => (
-    <div>
-      <Helmet
-        title="Gatsby Default Starter"
-        meta={[
-          { name: 'description', content: 'Sample' },
-          { name: 'keywords', content: 'sample, something' },
-        ]}
-      />
-      <Header />
-      <div
-        style={{
-          margin: '0 auto',
-          maxWidth: 960,
-          padding: '0px 1.0875rem 1.45rem',
-          paddingTop: 0,
-        }}
-      >
-        {children()}
-      </div>
+const TemplateWrapper = ({ children }) =>
+  <div>
+    <Helmet
+      title="Gatsby Default Starter"
+      meta={[
+        { name: 'description', content: 'Sample' },
+        { name: 'keywords', content: 'sample, something' },
+      ]}
+    />
+    <Header />
+    <div
+      style={{
+        margin: '0 auto',
+        maxWidth: 960,
+        padding: '0px 1.0875rem 1.45rem',
+        paddingTop: 0,
+      }}
+    >
+      {children()}
     </div>
-  )
+  </div>
 
 TemplateWrapper.propTypes = {
   children: PropTypes.func,
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
-const SecondPage = () => (
+const SecondPage = () =>
   <div>
     <h1>Hi people</h1>
     <p>Welcome to page 2</p>
     <Link to="/">Go back to the homepage</Link>
   </div>
-)
 
 export default SecondPage
