249121e0dbf3301d0e764b2a3437c078b141f0c3
Marco Biedermann <marco.biedermann@gmx.de>
2018-01-10 16:42:55 +0100

    Feature: Clean up code base (#40)
    * fix auto fixable ESLint warnings
    
    * use .jsx file extension
    
    * move Header into it's own component
    
    * use .js insteda of .jsx file extension
    
    * format code using prettier

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,38 +1,9 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import Link from 'gatsby-link'
 import Helmet from 'react-helmet'
 
+import Header from '../components/Header'
 import './index.css'
-
-const Header = () => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          Gatsby
-        </Link>
-      </h1>
-    </div>
-  </div>
-)
 
 const TemplateWrapper = ({ children }) => (
   <div>
