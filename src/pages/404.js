b74566bf129c845093a710be373b1e14c624f607
pravdomil <pravdomil.toman@gmail.com>
2018-05-27 00:08:55 +0200

    Update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,8 @@
 import React from 'react'
 
-const NotFoundPage = () => (
+export default () => (
   <div>
     <h1>NOT FOUND</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
   </div>
 )
-
-export default NotFoundPage
