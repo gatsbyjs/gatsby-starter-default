8a66010a34229d732218f08bc6ce37e9bad615f8
Jason Lengstorf <jason@gatsbyjs.com>
2018-06-20 09:28:38 -0700

    fix: remove unnecessary fragment

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,12 +5,10 @@ import Layout from '../components/layout'
 
 const IndexPage = () => (
   <Layout>
-    <>
-      <h1>Hi people</h1>
-      <p>Welcome to your new Gatsby site.</p>
-      <p>Now go build something great.</p>
-      <Link to="/page-2/">Go to page 2</Link>
-    </>
+    <h1>Hi people</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
