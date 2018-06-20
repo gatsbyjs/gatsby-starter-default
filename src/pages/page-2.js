f023a6374fe65cc2a02b9836cb73fef8ef7c39e5
Jason Lengstorf <jason@gatsbyjs.com>
2018-06-20 09:29:06 -0700

    fix: remove unnecessary fragment

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -5,11 +5,9 @@ import Layout from '../components/layout'
 
 const SecondPage = () => (
   <Layout>
-    <>
-      <h1>Hi from the second page</h1>
-      <p>Welcome to page 2</p>
-      <Link to="/">Go back to the homepage</Link>
-    </>
+    <h1>Hi from the second page</h1>
+    <p>Welcome to page 2</p>
+    <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
 
