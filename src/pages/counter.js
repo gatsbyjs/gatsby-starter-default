15d1847c3177506d8022a6d64db773c5963a8aef
Ola Vea <ola@olavea.com>
2018-06-25 11:19:22 +0200

    Update counter.js

--- src/pages/counter.js
+++ src/pages/counter.js
@@ -2,8 +2,15 @@ import React from "react";
 
 class Counter extends React.Component {
   render() {
-    return <div>Hello Class Component</div>;
+    return (
+      <div>
+        <h1>Counter</h1>
+        <p>current count: 0</p>
+        <button>plus</button>
+        <button>minus</button>
+      </div>
+    )
   }
 }
 
-export default Counter;
+export default Counter
