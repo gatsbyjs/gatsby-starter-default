5730edd99872d9830fe9f18e4f1b9ebb8effbeee
Ola Vea <ola@olavea.com>
2018-06-25 10:59:44 +0200

    Hello Gatsby!

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,12 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Hi people</h1>
+    <h1>Hello Gatsby!</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <Link to="/page-2/">Go to page 2</Link>
   </div>
 )
+
 
 export default IndexPage
