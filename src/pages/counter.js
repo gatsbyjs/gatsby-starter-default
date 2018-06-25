61a462fa49d6bd261af96aab39e942490662d6eb
Ola Vea <ola@olavea.com>
2018-06-25 11:20:25 +0200

    Update counter.js

--- src/pages/counter.js
+++ src/pages/counter.js
@@ -1,11 +1,16 @@
 import React from "react";
 
 class Counter extends React.Component {
+  constructor() {
+    super()
+    this.state = { count: 0 }
+  }
+
   render() {
     return (
       <div>
         <h1>Counter</h1>
-        <p>current count: 0</p>
+        <p>current count: {this.state.count}</p>
         <button>plus</button>
         <button>minus</button>
       </div>
