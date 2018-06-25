a1a9d50f5f2877b4eefe31fefb01123d6d2a9d58
Ola Vea <ola@olavea.com>
2018-06-25 11:11:40 +0200

    Create page-1.js

---
+++ src/components/page-1.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Link from "gatsby-link";
+
+export default () =>
+  <div style={{ color: `tomato` }}>
+    <h1>Hello Gatsby!</h1>
+    <p>What a world.</p>
+    <img src="https://source.unsplash.com/random/400x200" alt="" />
+    <br />
+    <div>
+      <Link to="/page-2/">Link</Link>
+    </div>
+  </div>
