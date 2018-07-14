894d08953c433685fc34e734c6f1c67a8c02f6a3
Hamid Adelyar <hamidadelyar@gmail.com>
2018-07-14 20:11:41 +0100

    corrected missing div tag in layout component

--- src/components/layout.js
+++ src/components/layout.js
@@ -18,7 +18,7 @@ const Layout = ({ children, data }) => (
       }
     `}
     render={data => (
-      <>
+      <div>
         <Helmet
           title={data.site.siteMetadata.title}
           meta={[
@@ -37,7 +37,7 @@ const Layout = ({ children, data }) => (
         >
           {children}
         </div>
-      </>
+      </div>
     )}
   />
 )
