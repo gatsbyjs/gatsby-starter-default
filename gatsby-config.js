ed63570aef12b75c7482b710b80cb2f69ce4bb73
Nick Roach <roachnt@miamioh.edu>
2018-07-21 21:16:00 -0400

    add plugins to config

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,20 @@ module.exports = {
   siteMetadata: {
     title: 'Gatsby Default Starter',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'gatsby-starter-default',
+        short_name: 'starter',
+        start_url: '/',
+        background_color: '#f7f0eb',
+        theme_color: '#a2466c',
+        display: 'minimal-ui',
+        // icon: 'src/images/icon.png', // This path is relative to the root of the site.
+      },
+    },
+    'gatsby-plugin-offline',
+  ],
 }
