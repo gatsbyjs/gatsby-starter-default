3f247b3433af54ef42142f98304ad028f791a7b9
Fei Liu <liu.feiwood@gmail.com>
2018-10-17 14:42:12 +0200

    Replace unneeded template literal with normal string

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: 'gatsby-plugin-manifest',
       options: {
         name: 'gatsby-starter-default',
         short_name: 'starter',
