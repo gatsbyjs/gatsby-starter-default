4bda9b52ee337352a0e693679725cba766a63dd4
diegomadeit <diegomadeit@gmail.com>
2018-10-22 20:04:41 +0200

    First commit
    Example Gatsby

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import Layout from '../components/layout'
 
 const IndexPage = () => (
   <Layout>
-    <h1>Hi people</h1>
+    <h1>Hi people this is Diego</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <Link to="/page-2/">Go to page 2</Link>
