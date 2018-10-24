3b12764c7539208b2c56a27a39458885e90179db
Jason Lengstorf <jason@gatsbyjs.com>
2018-10-23 17:07:38 -0700

    doc: add more detail to comment

--- src/components/image.js
+++ src/components/image.js
@@ -2,7 +2,16 @@ import React from 'react'
 import { StaticQuery, graphql } from 'gatsby'
 import Img from 'gatsby-image'
 
-/* This component loads images using 'gatsby-image'. Read more: https://github.com/gatsbyjs/gatsby/tree/master/packages/gatsby-image */
+/*
+ * This component is built using `gatsby-image` to automatically serve optimized
+ * images with lazy loading and reduced file sizes. The image is loaded using a
+ * `StaticQuery`, which allows us to load the image from directly within this
+ * component, rather than having to pass the image data down from pages.
+ *
+ * For more information, see the docs:
+ * - `gatsby-image`: https://gatsby.app/gatsby-image
+ * - `StaticQuery`: https://gatsby.app/staticquery
+ */
 
 const Image = () => (
   <StaticQuery
