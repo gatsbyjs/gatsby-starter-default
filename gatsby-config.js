3c530b288c71c3b44c43a272b22f9e166bb617c1
Dustin Schau <DSchau@users.noreply.github.com>
2018-11-05 12:27:35 -0600

    fix: make service worker/pwa opt-in and comment gatsby-plugin-offline (#113)
    * chore: comment out gatsby-plugin-offline; add message
    
    * chore: add short link

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,6 +25,8 @@ module.exports = {
         icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
     },
-    'gatsby-plugin-offline',
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.app/offline
+    // 'gatsby-plugin-offline',
   ],
 }
