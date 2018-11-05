c2370893e4f442b36bb39e9b84f3ada7f9f2737c
Dustin Schau <dustinschau@gmail.com>
2018-11-05 10:33:26 -0600

    chore: add short link

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,7 +26,7 @@ module.exports = {
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: <ADD_SHORT_LINK>
+    // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
   ],
 }
