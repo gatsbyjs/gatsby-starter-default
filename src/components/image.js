026a6f710e392f7a28da6b3a6c622e7ff76fd0f9
Dustin Horton <d@dustinhorton.com>
2018-11-07 21:35:56 -0600

    generate high resolution image

--- src/components/image.js
+++ src/components/image.js
@@ -19,7 +19,7 @@ const Image = () => (
       query {
         placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
           childImageSharp {
-            fluid(maxWidth: 300) {
+            fluid(maxWidth: 600) {
               ...GatsbyImageSharpFluid
             }
           }
