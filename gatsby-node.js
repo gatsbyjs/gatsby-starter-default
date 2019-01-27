f9342cfee6384b32a13759387d4c31196e78c1d9
Ryan Townsend <rtownsend@aais.com>
2019-01-27 13:08:06 -0600

    example resftul api call
    quick and dirty demonstration of retrieving restful data using gastby's createNode (as outlined here: https://stackoverflow.com/questions/49299309/gatsbyjs-getting-data-from-restful-api)

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,54 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const axios = require('axios');
+const crypto = require('crypto');
 
-// You can delete this file if you're not using it
+exports.sourceNodes = async ({ boundActionCreators }) => {
+  const { createNode } = boundActionCreators;
+
+  // fetch raw data from the randomuser api
+  const fetchRandomUser = () => axios.get(`https://randomuser.me/api/?results=500`);
+  // await for results
+  const res = await fetchRandomUser();
+
+  // map into these results and create nodes
+  res.data.results.map((user, i) => {
+    // Create your node object
+    const userNode = {
+      // Required fields
+      id: `${i}`,
+      parent: `__SOURCE__`,
+      internal: {
+        type: `RandomUser`, // name of the graphQL query --> allRandomUser {}
+        // contentDigest will be added just after
+        // but it is required
+      },
+      children: [],
+
+      // Other fields that you want to query with graphQl
+      gender: user.gender,
+      name: {
+        title: user.name.title,
+        first: user.name.first,
+        last: user.name.last,
+      },
+      picture: {
+        large: user.picture.large,
+        medium: user.picture.medium,
+        thumbnail: user.picture.thumbnail,
+      }
+      // etc...
+    }
+
+    // Get content digest of node. (Required field)
+    const contentDigest = crypto
+      .createHash(`md5`)
+      .update(JSON.stringify(userNode))
+      .digest(`hex`);
+    // add it to userNode
+    userNode.internal.contentDigest = contentDigest;
+
+    // Create node with the gatsby createNode() API
+    createNode(userNode);
+  });
+
+  return;
+}
