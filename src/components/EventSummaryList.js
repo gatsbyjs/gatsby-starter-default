776943a3474503ec9db9b32cc034c15303d5d21d
Ryan Townsend <rtownsend@aais.com>
2019-02-11 03:50:09 -0600

    added wait spinner
    wait spinner now shows while fetching data from server

--- src/components/EventSummaryList.js
+++ src/components/EventSummaryList.js
@@ -3,6 +3,15 @@ import styled from 'styled-components';
 import EventSummary from '../containers/EventSummary';
 import H1 from './H1';
 
+import { css } from '@emotion/core';
+import { PulseLoader, RingLoader, SyncLoader } from 'react-spinners';
+
+const override = css`    
+    margin: 0 auto;
+    display: inline;
+    margin-left: 20px;
+`;
+
 const EventSummaryListWrapper = styled.ul`
   list-style-type: none;
   margin-top: 0;
@@ -15,11 +24,24 @@ const EventSummaryListWrapper = styled.ul`
   li:selected {
     background-color: #A2D539;
   }
+  
+  H1 {
+    display: inline;
+  }
 `;
 
 const EventSummaryList = ({ activities }) => (
   <EventSummaryListWrapper>
     <H1>All Events</H1>
+    <span className='sweet-loading'>
+      <PulseLoader            
+        css={override}
+        sizeUnit={"px"}
+        size={10}
+        color={'#1b75b7'}
+        loading={activities.loading}
+      />
+    </span>
     {activities.activities.map((activity, i) => (       
       <EventSummary key={i} eventData={activity}></EventSummary>  
      ))}
