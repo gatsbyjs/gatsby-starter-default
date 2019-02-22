71afccc99df5ad25becc3b6f68ef670c0bc8c661
Dylan Tackoor <dylan@worldmedia.net>
2019-02-22 14:20:22 -0500

    Revert "Indentation fix"
    This reverts commit eab0abba731c904d58d89f4a057500a114f4c62a.

--- src/components/seo.js
+++ src/components/seo.js
@@ -4,96 +4,96 @@ import Helmet from 'react-helmet'
 import { graphql, useStaticQuery } from 'gatsby'
 
 function SEO({ description, lang, meta, keywords, title }) {
-  const { site } = useStaticQuery(graphql`
-    query DefaultSEOQuery {
-      site {
-        siteMetadata {
-          title
-          description
-          author
-        }
-      }
-    }
-  `)
+	const { site } = useStaticQuery(graphql`
+		query DefaultSEOQuery {
+			site {
+				siteMetadata {
+					title
+					description
+					author
+				}
+			}
+		}
+	`)
 
-  return (
-    <Helmet
-      htmlAttributes={{
-        lang,
-      }}
-      title={title}
-      titleTemplate={`%s | ${siteMetadata.title}`}
-      meta={[
-        {
-          name: `description`,
-          content: metaDescription,
-        },
-        {
-          property: `og:title`,
-          content: title,
-        },
-        {
-          property: `og:description`,
-          content: metaDescription,
-        },
-        {
-          property: `og:type`,
-          content: `website`,
-        },
-        {
-          name: `twitter:card`,
-          content: `summary`,
-        },
-        {
-          name: `twitter:creator`,
-          content: siteMetadata.author,
-        },
-        {
-          name: `twitter:title`,
-          content: title,
-        },
-        {
-          name: `twitter:description`,
-          content: metaDescription,
-        },
-      ]
-        .concat(
-          keywords.length > 0
-            ? {
-                name: `keywords`,
-                content: keywords.join(`, `),
-              }
-            : []
-        )
-        .concat(meta)}
-    />
-  )
+	return (
+		<Helmet
+			htmlAttributes={{
+				lang,
+			}}
+			title={title}
+			titleTemplate={`%s | ${siteMetadata.title}`}
+			meta={[
+				{
+					name: `description`,
+					content: metaDescription,
+				},
+				{
+					property: `og:title`,
+					content: title,
+				},
+				{
+					property: `og:description`,
+					content: metaDescription,
+				},
+				{
+					property: `og:type`,
+					content: `website`,
+				},
+				{
+					name: `twitter:card`,
+					content: `summary`,
+				},
+				{
+					name: `twitter:creator`,
+					content: siteMetadata.author,
+				},
+				{
+					name: `twitter:title`,
+					content: title,
+				},
+				{
+					name: `twitter:description`,
+					content: metaDescription,
+				},
+			]
+				.concat(
+					keywords.length > 0
+						? {
+								name: `keywords`,
+								content: keywords.join(`, `),
+						  }
+						: []
+				)
+				.concat(meta)}
+		/>
+	)
 }
 
 SEO.defaultProps = {
-  lang: `en`,
-  meta: [],
-  keywords: [],
+	lang: `en`,
+	meta: [],
+	keywords: [],
 }
 
 SEO.propTypes = {
-  description: PropTypes.string,
-  lang: PropTypes.string,
-  meta: PropTypes.array,
-  keywords: PropTypes.arrayOf(PropTypes.string),
-  title: PropTypes.string.isRequired,
+	description: PropTypes.string,
+	lang: PropTypes.string,
+	meta: PropTypes.array,
+	keywords: PropTypes.arrayOf(PropTypes.string),
+	title: PropTypes.string.isRequired,
 }
 
 export default SEO
 
 const detailsQuery = graphql`
-  query DefaultSEOQuery {
-    site {
-      siteMetadata {
-        title
-        description
-        author
-      }
-    }
-  }
+	query DefaultSEOQuery {
+		site {
+			siteMetadata {
+				title
+				description
+				author
+			}
+		}
+	}
 `
