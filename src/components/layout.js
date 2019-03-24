58d6f2ba99d4c953a68eaeeeb1c4ecb9f7cbe9bc
wiziple <wiziple@gmail.com>
2019-03-24 09:24:45 -0700

    Refactor footer template literals

--- src/components/layout.js
+++ src/components/layout.js
@@ -36,8 +36,7 @@ const Layout = ({ children }) => (
         >
           <main>{children}</main>
           <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
+            {`© ${new Date().getFullYear()}, Built with `} 
             <a href="https://www.gatsbyjs.org">Gatsby</a>
           </footer>
         </div>
