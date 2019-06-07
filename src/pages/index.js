eade028fe8cb3520ad100f9aea8e07225f338c4c
Jen Reiher <jreiher@benevity.com>
2019-06-07 15:02:41 -0700

    Add explanatory paragraph

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ import '../styles/main.scss'
 const IndexPage = ({siteDescription}) => (
   <Layout>
     <SEO title="YYJ Tech" />
-    { siteDescription }
+    <p>We're a self-organizing slack community for people who are looking to discuss topics related to technology + Victoria, BC. </p>
     <Link
       to="https://join.slack.com/t/yyjtech/shared_invite/enQtNjQ3NTkzMDg2MDk4LTI4NDdiNjJjNTRmMmI0NTlmNzJkNDg2OWJmNjY5N2IwZWQxMTRkOWNkZTMzYmExMzE1NmY2ZWJmYjY3ZDg1NTc"
       className="button is-primary"
