5522b9325c7ebb0831fd130edbb62be4b0786a24
jenreiher <jenreiher@users.noreply.github.com>
2019-08-11 13:00:53 -0700

    add calendar container

--- src/pages/calendar.js
+++ src/pages/calendar.js
@@ -9,8 +9,10 @@ const CalendarPage = () => (
   <Layout>
     <div className="flex-center-column">
       <SEO title="YYJ Tech Events" />
-      <a href="http://bit.ly/yyjtech-events" className="button" target="_blank">Add an event</a> 
-      <iframe src="https://calendar.google.com/calendar/embed?src=techyyj@gmail.com&ctz=America%2FVancouver" className="styleCalendar"></iframe>
+      <div className="calendarContainer">
+        <a href="http://bit.ly/yyjtech-events" className="button" target="_blank">Add an event</a> 
+        <iframe src="https://calendar.google.com/calendar/embed?src=techyyj@gmail.com&ctz=America%2FVancouver" className="styleCalendar"></iframe>
+      </div>
   </div>
   </Layout>
 )
