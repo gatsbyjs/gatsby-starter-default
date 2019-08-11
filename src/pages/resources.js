d17e62d3f8b44f767cd8c8d11427a8096a39cfda
jenreiher <jenreiher@users.noreply.github.com>
2019-08-11 12:06:57 -0700

    Update resources.js

--- src/pages/resources.js
+++ src/pages/resources.js
@@ -8,10 +8,12 @@ import '../styles/main.scss'
 const IndexPage = ({siteDescription}) => (
   <Layout>
     <div className="flex-center-column">
-      <SEO title="YYJ Tech Resources" />
-      <h2>Jobs</h2>
-      
-      <h2>Events & Networking</h2>
+      <div className="box">
+          <SEO title="YYJ Tech Resources" />
+          <h2>Jobs</h2>
+
+          <h2>Events & Networking</h2>
+      </div>
     </div>
   </Layout>
 )
