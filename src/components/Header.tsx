60b135adfa0e7f6c490b8eefa64105212e54fd59
Stephen Mccarrick <smccarrick2804@gmail.com>
2019-09-26 21:33:54 +0100

    Initial commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => (
+  <header>
+    <div>
+      <h3>Follow my Journey</h3>
+    </div>
+  </header>
+);
+
+export default Header;
---
+++ src/components/Layout.tsx
@@ -0,0 +1,33 @@
+import React, { ReactNode } from 'react';
+
+import Header from './Header';
+import './layout.css';
+
+interface Props {
+  children: ReactNode;
+}
+
+const Layout = ({ children }: Props) => {
+  return (
+    <>
+      <Header />
+      <div
+        style={{
+          margin: `0 auto`,
+          maxWidth: 960,
+          padding: `0px 1.0875rem 1.45rem`,
+          paddingTop: 0,
+        }}
+      >
+        <main>{children}</main>
+        <footer>
+          © {new Date().getFullYear()}, Built with
+          {` `}
+          <a href="https://www.gatsbyjs.org">Gatsby</a>
+        </footer>
+      </div>
+    </>
+  );
+};
+
+export default Layout;
