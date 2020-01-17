8bf71bc1c8ec6786c7d35c1e137d3cf458aba5fe
Bryan Robinson <bryanlrobinson@gmail.com>
2020-01-17 12:42:42 -0600

    Removes extraneous paddingTop value handled by the padding shorthand

--- src/components/layout.js
+++ src/components/layout.js
@@ -31,7 +31,6 @@ const Layout = ({ children }) => {
           margin: `0 auto`,
           maxWidth: 960,
           padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
         }}
       >
         <main>{children}</main>
