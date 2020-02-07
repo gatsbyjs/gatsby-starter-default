91db44ac482fa7bba9fb30ab692ba58097d2529e
Payton O'Neal <payton@Paytons-MacBook-Pro.local>
2020-02-07 15:50:48 -0800

    Adding percy snapshots

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,6 +15,7 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    `gatsby-plugin-percy`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
