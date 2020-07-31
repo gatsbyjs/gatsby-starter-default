decea8e8e1445a9b8ab9069b50b83dc225149043
SaRyo1204 <68261037+SaRyo1204@users.noreply.github.com>
2020-07-31 12:11:02 +0900

    initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,6 +11,7 @@ const IndexPage = () => (
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
+    <p>hello world.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
