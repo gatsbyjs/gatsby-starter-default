ec5543768c63e030242fb8d9016e616a9f61f9ef
kurikinton105 <51431248+kurikinton105@users.noreply.github.com>
2020-08-03 20:17:02 +0900

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ const IndexPage = () => (
     <SEO title="Home" />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
-    <p>yamaだよ！！！Azureのプレビューサイトのやつだよ！！<\p>
+    <p>yamaだよ！！！Azureのプレビューサイトのやつだよ！！！！</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
