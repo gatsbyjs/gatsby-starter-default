b43c9ebb0da8a24eee07020b8aedb1367bbafe88
DyWhy <83489533+DyWhy@users.noreply.github.com>
2021-09-20 20:14:00 +0530

    Update index.js
    i think this project is made for developers .

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ import Seo from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <h1>Hi people</h1>
+    <h1>Hi Developer</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <StaticImage
