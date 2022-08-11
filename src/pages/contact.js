3776e0b9671d1566c7e7ce519950abe600be0827
chloepomeroy <pomeroyc@outlook.com>
2022-08-11 10:19:31 -0400

    contact success

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -8,6 +8,10 @@ import { styled } from '@mui/material/styles';
 import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
 import Stack from '@mui/material/Stack';
+import Alert from '@mui/material/Alert';
+import IconButton from '@mui/material/IconButton';
+import Collapse from '@mui/material/Collapse';
+import CloseIcon from '@mui/icons-material/Close';
 
 const Item = styled(Paper)(({ theme }) => ({
     backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -18,6 +22,17 @@ const Item = styled(Paper)(({ theme }) => ({
   }));
 
 const Contact = () => {
+    const [successOpen, setSuccessOpen] = React.useState(false);
+    const [errorOpen, setErrorOpen] = React.useState(false);
+
+    const Submit = event => {
+      //need to add logic to actually submit the question
+      
+      //if success
+      setSuccessOpen(true);
+      //if error
+      // setErrorOpen(true);
+    };
 
   return(
     <Layout>
@@ -28,9 +43,55 @@ const Contact = () => {
                     <h2>Contact Support</h2>
                 </Item>
             </Grid>
+
+{/* Success alert */}
+            <Box sx={{ width: '100%' }}>
+              <Collapse in={successOpen}>
+                <Alert
+                action={
+                  <IconButton
+                    aria-label="close"
+                    color="inherit"
+                    size="small"
+                    onClick={() => {
+                      setSuccessOpen(false);
+                    }}
+                  >
+                    <CloseIcon fontSize="inherit" />
+                  </IconButton>
+                  }
+                  sx={{ mb: 2 }}
+                >
+                Your question has been submitted!
+                </Alert>
+              </Collapse>
+            </Box>
+{/* Error Alert */}
+            <Box sx={{ width: '100%' }}>
+              <Collapse in={errorOpen}>
+                <Alert severity = "error"
+                action={
+                  <IconButton
+                    aria-label="close"
+                    color="inherit"
+                    size="small"
+                    onClick={() => {
+                      setErrorOpen(false);
+                    }}
+                  >
+                    <CloseIcon fontSize="inherit" />
+                  </IconButton>
+                  }
+                  sx={{ mb: 2 }}
+                >
+                There was an error submitting your question! Please call (number) or email (email) directly.
+                </Alert>
+              </Collapse>
+            </Box>
+
             <Grid item xs={10}>
             <Box m="auto">
-                <Stack justifyContent="center" spacing={2}>
+                <Stack justifyContent="center" spacing={2}>              
                     <TextField fullWidth id="outlined-basic" label="Subject" variant="outlined" margin="normal"/>
                     <TextField
                     fullWidth
@@ -42,13 +103,13 @@ const Contact = () => {
                     margin="normal"
                     />
                     <Stack direction="row" justifyContent="center" spacing={3} pt={4}>
-                        <Button variant="contained" color="success" href="#contained-buttons" >
+                        <Button variant="contained" color="success" onClick={Submit} >
                             Submit
                         </Button>
                         <Button variant="contained" color="success" href="/faq">
                             Cancel
                         </Button>
-                    </Stack>
+                    </Stack>  
                 </Stack>
             </Box>
             </Grid>
