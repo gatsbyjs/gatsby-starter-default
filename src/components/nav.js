0c5aad5a03215b1943a8ccc5d016d15d18a12fb9
Dario Costantini <d.costantini@multi-consult.it>
2024-07-04 12:13:53 +0200

    adding mobile and framer motion for the boiler plate

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,6 +1,5 @@
-import { Box, Container, Flex } from "@theme-ui/components"
 import React, { useState, useContext } from "react"
-
+import { Box, Container, Flex } from "@theme-ui/components"
 import { getHomePath, getSearchPath } from "../utils/path"
 import { InboundLink } from "./link"
 import { MagicLink } from "../utils/magicLink"
@@ -8,10 +7,12 @@ import LanguageSwitcher from "./languageSwitcher"
 import { debounce } from "lodash"
 import { LanguageSwitcherContext } from "../context/languageSwitcherContext"
 import { MenuContext } from "../context/menuContext"
-const Nav = ({}) => {
+import { useEffect } from "react"
+
+const Nav = () => {
   const menu = useContext(MenuContext)
-  console.log(menu)
-  const locale = React.useContext(LanguageSwitcherContext).activeLocale
+  const { activeLocale: locale } = useContext(LanguageSwitcherContext)
+
   menu.map(menuItem => {
     menuItem.treeChildren.sort((a, b) => a.position - b.position)
     menuItem.treeChildren.map(menuItem => {
@@ -28,92 +29,102 @@ const Nav = ({}) => {
           <Box sx={{ paddingX: 2, paddingY: 3 }}>
             <InboundLink to={getHomePath(locale)}>Home</InboundLink>
           </Box>
-          <Flex
-            sx={{
-              flexDirection: "row",
-              padding: 0,
-              margin: 0,
-              listStyle: "none",
-            }}
-            as="ul"
-          >
-            {menu.map(item => (
-              <TextComponent item={item} locale={locale} key={item.id} />
-            ))}
-          </Flex>
-          <Flex
-            sx={{
-              flexDirection: "row",
-              padding: 3,
-              margin: 0,
-              listStyle: "none",
-            }}
-          >
-            <InboundLink to={getSearchPath(locale)}>Search</InboundLink>
-            <LanguageSwitcher />
-          </Flex>
+          <MenuItems menu={menu} locale={locale} />
+          <SearchAndLanguageSwitcher locale={locale} />
         </Flex>
       </Container>
     </Box>
   )
 }
 
-const TextComponent = ({ item, locale, index }) => {
+const MenuItems = ({ menu, locale }) => (
+  <Flex
+    sx={{
+      flexDirection: "row",
+      padding: 0,
+      margin: 0,
+      listStyle: "none",
+    }}
+    as="ul"
+  >
+    {menu.map(item => (
+      <TextComponent item={item} locale={locale} key={item.id} />
+    ))}
+  </Flex>
+)
+
+const TextComponent = ({ item, locale }) => {
   const [show, setShow] = useState(false)
-
-  const debouncedHandleMouseEnterMenu = debounce(() => {
-    setShow(true)
-  }, 200)
-
-  const handlOnMouseLeaveMenu = () => {
+  const debouncedHandleMouseEnterMenu = debounce(() => setShow(true), 200)
+  const handleOnMouseLeaveMenu = () => {
     setShow(false)
     debouncedHandleMouseEnterMenu.cancel()
   }
-  console.log(locale)
+
+  useEffect(() => {
+    return () => {
+      debouncedHandleMouseEnterMenu.cancel()
+    }
+  }, [debouncedHandleMouseEnterMenu])
+
   return (
     <Box
       as="li"
-      key={item.id}
       sx={{ position: "relative", marginX: 1, paddingX: 2, paddingY: 3 }}
-      onMouseEnter={() => debouncedHandleMouseEnterMenu()}
-      onMouseLeave={() => handlOnMouseLeaveMenu()}
+      onMouseEnter={debouncedHandleMouseEnterMenu}
+      onMouseLeave={handleOnMouseLeaveMenu}
     >
-      {item.link ? (
-        <MagicLink item={item.link} locale={locale}></MagicLink>
-      ) : (
-        <Box sx={{ cursor: "default" }}>{item.anchor}</Box>
-      )}
-      {item.treeChildren.length > 0 && show && (
-        <Box
-          as="ul"
-          sx={{
-            listStyle: "none",
-            padding: 3,
-            margin: 0,
-            backgroundColor: "lightBackground",
-            position: "absolute",
-            top: 40,
-            width: "max-content",
-            boxShadow:
-              "0 4px 6px rgb(50 50 93 / 11%), 0 1px 3px rgb(0 0 0 / 8%)",
-            borderRadius: "sm",
-          }}
-        >
-          {item.treeChildren.map(item =>
-            item.anchor ? (
-              <Box as="li" key={item.id}>
-                {item.link ? (
-                  <MagicLink item={item.link} locale={locale} />
-                ) : (
-                  <Box sx={{ cursor: "default" }}>{item.anchor}</Box>
-                )}
-              </Box>
-            ) : null
-          )}
-        </Box>
-      )}
+      <MagicLinkOrAnchor item={item} locale={locale} />
+      {show && <SubMenu items={item.treeChildren} locale={locale} />}
     </Box>
   )
 }
+
+const SubMenu = ({ items, locale }) => (
+  <Box
+    as="ul"
+    sx={{
+      listStyle: "none",
+      padding: 3,
+      margin: 0,
+      backgroundColor: "lightBackground",
+      position: "absolute",
+      top: 40,
+      width: "max-content",
+      boxShadow: "0 4px 6px rgb(50 50 93 / 11%), 0 1px 3px rgb(0 0 0 / 8%)",
+      borderRadius: "sm",
+    }}
+  >
+    {items.map(
+      item =>
+        item.anchor && (
+          <Box as="li" key={item.id}>
+            <MagicLinkOrAnchor item={item} locale={locale} />
+          </Box>
+        )
+    )}
+  </Box>
+)
+
+const MagicLinkOrAnchor = ({ item, locale }) =>
+  item.link ? (
+    <MagicLink item={item.link} locale={locale} />
+  ) : (
+    <Box sx={{ cursor: "default" }}>{item.anchor}</Box>
+  )
+
+const SearchAndLanguageSwitcher = ({ locale }) => (
+  <Flex
+    sx={{
+      flexDirection: "row",
+      padding: 3,
+      margin: 0,
+      listStyle: "none",
+    }}
+  >
+    <InboundLink to={getSearchPath(locale)}>Search</InboundLink>
+    <LanguageSwitcher />
+  </Flex>
+)
 
 export default Nav
